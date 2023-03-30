234c479c3a6876be4bb0d21fd2db531046fac9dc
Renier Collazo Woo <rcollazo@emergya.com>
2023-03-29 23:53:53 -0400

    Update providers and variables

--- providers.tf
+++ providers.tf
@@ -24,6 +24,15 @@ terraform {
       version = "1.14.0"
     }
   }
+
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "wootechspace"
+
+    workspaces {
+      name = "nexus-k8s"
+    }
+  }
 }
 
 ###########################
@@ -35,43 +44,54 @@ provider "null" {
 }
 
 provider "kubernetes" {
-  host                     = var.host
-  client_certificate       = base64decode(var.client_certificate)
-  client_key               = base64decode(var.client_key)
-  cluster_ca_certificate   = base64decode(var.cluster_ca_certificate)
+  # Config for Terraform Cloud
+  # host                     = var.host
+  # client_certificate       = base64decode(var.client_certificate)
+  # client_key               = base64decode(var.client_key)
+  # cluster_ca_certificate   = base64decode(var.cluster_ca_certificate)
   # config_context           = var.config_context
   # config_context_auth_info = var.config_context_auth_info
   # config_context_cluster   = var.config_context_cluster
-  # config_path              = var.kubeconfig_path
-  # config_context           = var.kubeconfig_context
-  # config_context_cluster   = var.kubeconfig_cluster
-  # config_context_auth_info = var.kubeconfig_user
+
+  # Config for Terraform Local
+  config_path              = var.kubeconfig_path
+  config_context           = var.kubeconfig_context
+  config_context_cluster   = var.kubeconfig_cluster
+  config_context_auth_info = var.kubeconfig_user
 }
 
 provider "helm" {
   kubernetes {
-    host                     = var.host
-    client_certificate       = base64decode(var.client_certificate)
-    client_key               = base64decode(var.client_key)
-    cluster_ca_certificate   = base64decode(var.cluster_ca_certificate)
+    # Config for Terraform Cloud
+    # host                     = var.host
+    # client_certificate       = base64decode(var.client_certificate)
+    # client_key               = base64decode(var.client_key)
+    # cluster_ca_certificate   = base64decode(var.cluster_ca_certificate)
     # config_context           = var.config_context
     # config_context_auth_info = var.config_context_auth_info
     # config_context_cluster   = var.config_context_cluster
-    # config_path    = var.kubeconfig_path
-    # config_context = var.kubeconfig_context
+
+    # Config for Terraform Local
+    config_path              = var.kubeconfig_path
+    config_context           = var.kubeconfig_context
+    config_context_cluster   = var.kubeconfig_cluster
+    config_context_auth_info = var.kubeconfig_user
   }
 }
 
 provider "kubectl" {
-  host                     = var.host
-  client_certificate       = base64decode(var.client_certificate)
-  client_key               = base64decode(var.client_key)
-  cluster_ca_certificate   = base64decode(var.cluster_ca_certificate)
+  # Config for Terraform Cloud
+  # host                     = var.host
+  # client_certificate       = base64decode(var.client_certificate)
+  # client_key               = base64decode(var.client_key)
+  # cluster_ca_certificate   = base64decode(var.cluster_ca_certificate)
   # config_context           = var.config_context
   # config_context_auth_info = var.config_context_auth_info
   # config_context_cluster   = var.config_context_cluster
-  # config_path              = var.kubeconfig_path
-  # config_context           = var.kubeconfig_context
-  # config_context_cluster   = var.kubeconfig_cluster
-  # config_context_auth_info = var.kubeconfig_user
+
+  # Config for Terraform Local
+  config_path              = var.kubeconfig_path
+  config_context           = var.kubeconfig_context
+  config_context_cluster   = var.kubeconfig_cluster
+  config_context_auth_info = var.kubeconfig_user
 }
--- variables.tf
+++ variables.tf
@@ -33,29 +33,30 @@ variable "hostname" {
 ##############################
 ### Kubeconfig's variables ###
 ##############################
+# Config for Terraform Local
+variable "kubeconfig_path" {
+  type        = string
+  # default     = "~/.kube/config"
+  description = "Location of the kubeconfig file belonging to the Kubernetes API server."
+  sensitive   = true
+}
 
-# variable "kubeconfig_path" {
-#   type        = string
-#   # default     = "~/.kube/config"
-#   description = "Location of the kubeconfig file belonging to the Kubernetes API server."
-#   sensitive   = true
-# }
+variable "kubeconfig_context" {
+  type        = string
+  description = "Context to choose from the kubeconfig file belonging to the Kubernetes API server."
+}
 
-# variable "kubeconfig_context" {
-#   type        = string
-#   description = "Context to choose from the kubeconfig file belonging to the Kubernetes API server."
-# }
+variable "kubeconfig_cluster" {
+  type        = string
+  description = "Cluster name to choose from the kubeconfig file belonging to the Kubernetes API server."
+}
 
-# variable "kubeconfig_cluster" {
-#   type        = string
-#   description = "Cluster name to choose from the kubeconfig file belonging to the Kubernetes API server."
-# }
+variable "kubeconfig_user" {
+  type        = string
+  description = "User name to choose from the kubeconfig file belonging to the Kubernetes API server."
+}
 
-# variable "kubeconfig_user" {
-#   type        = string
-#   description = "User name to choose from the kubeconfig file belonging to the Kubernetes API server."
-# }
-
+# Config for Terraform Cloud
 variable "host" {
   type        = string
   description = "The hostname (in form of URI) of the Kubernetes API."
