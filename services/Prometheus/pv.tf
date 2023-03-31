4ff88ad8f9e699b2c330503e0fc825a673653a8d
Renier Collazo Woo <rcollazo@emergya.com>
2023-03-31 13:31:32 -0400

    Update Prometheus Service

--- services/Prometheus/pv.tf
+++ services/Prometheus/pv.tf
@@ -19,15 +19,15 @@ resource "kubernetes_persistent_volume_v1" "prometheus" {
     }
 
     volume_mode = "Filesystem"
-  }
 
-  node_affinity {
-    required {
-      node_selector_terms {
-        match_expressions {
-          key      = "kubernetes.io/hostname"
-          operator = "In"
-          values   = [var.node_name]
+    node_affinity {
+      required {
+        node_selector_terms {
+          match_expressions {
+            key      = "kubernetes.io/hostname"
+            operator = "In"
+            values   = [var.node_name]
+          }
         }
       }
     }
